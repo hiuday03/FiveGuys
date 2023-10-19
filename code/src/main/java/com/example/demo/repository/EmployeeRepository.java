dd6ccd8662818da73ac874d68367ff53cd766d96
Quang_Tinh <nqtinh2003@gmail.com>
2023-10-19 23:27:22 +0700

    add voucher

--- code/src/main/java/com/example/demo/repository/EmployeeRepository.java
+++ code/src/main/java/com/example/demo/repository/EmployeeRepository.java
@@ -1,2 +1,9 @@
-package com.example.demo.repository;public interface EmployeeRepository {
+package com.example.demo.repository;
+
+import com.example.demo.entity.Employees;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employees, Long> {
 }
