35f2491bdb8c8baca83c120175cb77573f3abeb5
2R <haih70872@gmail.com>
2023-10-26 16:51:41 +0700

    Thêm service repo trong folder onlineSale

--- code/src/main/java/com/example/demo/repository/onlineSales/OLCategoryRepository.java
+++ code/src/main/java/com/example/demo/repository/onlineSales/OLCategoryRepository.java
@@ -1,9 +1,9 @@
 package com.example.demo.repository.onlineSales;
 
-import com.example.demo.entity.Bill;
+import com.example.demo.entity.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface BillOnlineSalesRepository extends JpaRepository<Bill, Long> {
+public interface OLCategoryRepository extends JpaRepository<Category, Long> {
 }
