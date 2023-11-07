bd62b9ef521b850e74e1adbe99d882f1b5767698
2R <haih70872@gmail.com>
2023-11-07 16:06:21 +0700

    config security đăng nhập basic

---
+++ code/src/main/java/com/example/demo/security/CustomUserDetails.java
@@ -0,0 +1,53 @@
+package com.example.demo.security;
+
+import com.example.demo.entity.AccountEntity;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Collections;
+
+public class CustomUserDetails implements UserDetails {
+
+    private final AccountEntity account;
+
+    public CustomUserDetails(AccountEntity account) {
+        this.account = account;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.singletonList(new SimpleGrantedAuthority( account.getRole().getFullName().toUpperCase()));
+    }
+
+    @Override
+    public String getPassword() {
+        return account.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return account.getAccount();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return account.getStatus() == 1;
+    }
+}
