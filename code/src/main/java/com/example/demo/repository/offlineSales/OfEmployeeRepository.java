ed96c75d6489d328f935a85e9b56ce76cc4992f3
2R <haih70872@gmail.com>
2023-11-20 12:41:34 +0700

    Thêm dòng để fix lỗi ofemoloyee

--- code/src/main/java/com/example/demo/repository/offlineSales/OfEmployeeRepository.java
+++ code/src/main/java/com/example/demo/repository/offlineSales/OfEmployeeRepository.java
@@ -7,4 +7,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface OfEmployeeRepository extends JpaRepository<Employees, Long> {
     Employees findByAccount_Id(Long accountId);
+
 }
