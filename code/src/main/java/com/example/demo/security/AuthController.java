9cf78cec0b6c0ac17de5ac163b62fdcb94829228
2R <haih70872@gmail.com>
2023-12-06 21:50:52 +0700

    fix nhẹ

--- code/src/main/java/com/example/demo/security/AuthController.java
+++ code/src/main/java/com/example/demo/security/AuthController.java
@@ -13,11 +13,12 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.Optional;
-
+@CrossOrigin("*")
 @Controller
 public class AuthController {
 
